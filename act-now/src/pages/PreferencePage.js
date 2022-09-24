22e711b65c2bea0966b9bf98793ccfba2e9dd4cf
Cuiyuanxiu Chen <cc127@rice.edu>
2022-09-24 16:24:12 -0500

    profile update

--- act-now/src/pages/PreferencePage.js
+++ act-now/src/pages/PreferencePage.js
@@ -8,13 +8,6 @@ export default function PreferencePage() {
             <h2 className='prefTitle mt-5'>What is Your Interest?</h2>
             <div className='subtitle'>Please select your interests (up to 5)</div>
             <form className='d-flex flex-column align-items-center'>
-                <div className='button-container'>
-                    <button className="button button1">Feminism</button>
-                    <button className="button button2">Environment</button>
-                    <button className="button button3">LGBTQ</button>
-                    <button className="button button4">Black Lives Matter</button>
-                    <button className="button button5">Equity and Inclusion</button>
-                </div>
 
                 <div className='button-container'>
                     <button className="button button1">Gender Equality</button>
