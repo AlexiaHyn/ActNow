166f07bf3ab1156dc288508d1c3855e96ec811c0
AlexiaHyn <sunfloweralexia@hotmail.com>
2022-09-25 00:50:02 -0500

    event card update

--- act-now/src/pages/MainPage.js
+++ act-now/src/pages/MainPage.js
@@ -5,11 +5,11 @@ import InitiateEvent from '../components/InitiateEvent';
 
 export default function MainPage() {
 
-  const recList = [{ "title": "Women's chat", "intro": "Come and join us for a great sister time!", "tags": "Gender Equality" },
-  { "title": "Tutor for grade 6-8 kids from low income family", "intro": "We host weekly tutor sessions for kids from low income families. We need grade 6-8 math, science, and English tutors.", "tags": "Education" },
-  { "title": "Mental Illness Awareness Association", "intro": "Join our organization meetings to find creative ways to help spread mental illness awareness across campus.", "tags": "Health" },
-  { "title": "Mural Painting for Pride Month", "intro": "Come paint the walls on campus to send positive messages to our community, in celebration of the pride month.", "tags": "LGPTQ+" },
-  { "title": "Climate March", "intro": "Join the Climate March on Sunday with your Rice classmates", "tags": "Environmental" }]
+  const recList = [{ "title": "Women's chat", "intro": "Come and join us for a great sister time!", "tags": ["Gender Equality"] },
+  { "title": "Tutor for grade 6-8 kids from low income family", "intro": "We host weekly tutor sessions for kids from low income families. We need grade 6-8 math, science, and English tutors.", "tags": ["Education"] },
+  { "title": "Mental Illness Awareness Association", "intro": "Join our organization meetings to find creative ways to help spread mental illness awareness across campus.", "tags": ["Health"] },
+  { "title": "Mural Painting for Pride Month", "intro": "Come paint the walls on campus to send positive messages to our community, in celebration of the pride month.", "tags":["LGPTQ+"] },
+  { "title": "Climate March", "intro": "Join the Climate March on Sunday with your Rice classmates", "tags": ["Environmental"] }]
 
   return (
     <div className='pt-5 Poppins'>
