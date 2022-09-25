616b8015457f21b1f6b5e697c8347dae239a48cc
AlexiaHyn <sunfloweralexia@hotmail.com>
2022-09-24 22:17:16 -0500

    initiate page

--- act-now/src/pages/EventPage.js
+++ act-now/src/pages/EventPage.js
@@ -11,9 +11,8 @@ export default function EventPage(props) {
 
 
             {/* <button type='button' className={'btn btn-outline-secondary'}>Back</button> */}
-            <div class=" h-100 d-flex justify-content-center align-items-center" >
-
-                <div className='card rounded event'>
+            <div className="white-background d-flex justify-content-center" >
+                <div className='event'>
                     <div className='d-flex justify-content-between'>
                         <h3>Event Name: {props.name}</h3>
                         <i className={"cursor bi bi-star" + `${collected ? "-fill" : ""}`} onClick={() => setCollected(!collected)}></i>
--- act-now/src/pages/InitiatePage.js
+++ act-now/src/pages/InitiatePage.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function InitiatePage() {
   return (
-    <div>InitiatePage</div>
+    <div className='white-background pt-5'>
+      <form className='pt-5 d-flex flex-column align-items-center'>
+        <div className="input-group mb-3 bg-dark" style={{fontSize: "25px", width: "70vw"}}>
+          <input type="text" className="input" style={{width: "100%"}}  placeholder="Event Title"/>
+        </div>
+
+      </form>
+    </div>
   )
 }
